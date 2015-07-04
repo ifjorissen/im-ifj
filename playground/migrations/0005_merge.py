80595226ee0c02028efcb146ccb6a982753fad8c
Isabella Jorissen <ifjorissen@gmail.com>
2015-07-04 15:04:06 -0700

    messy merge and templates

--- playground/migrations/0005_merge.py
+++ playground/migrations/0005_merge.py
@@ -8,12 +8,8 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('playground', '0002_auto_20150622_1716'),
+        ('playground', '0004_auto_20150619_1606'),
     ]
 
     operations = [
-        migrations.AlterField(
-            model_name='project',
-            name='thumbnail',
-            field=models.ImageField(null=True, upload_to='playground/%Y/%m/%d'),
-        ),
     ]
