27af7359ff8234879a2dd1fcc7e76ffd7bbca18c
Isabella Jorissen <ifjorissen@gmail.com>
2015-07-04 14:48:08 -0700

    added blog tags

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,27 @@
-from .models import Post
+from .models import Post, Tag
 from django.contrib import admin
+from django.forms import ModelForm
+
+class TagAdmin(admin.ModelAdmin):
+  prepopulated_fields = {"slug": ("content",)}
+
+class TagInline(admin.TabularInline):
+  model = Post.tags.through
+  extra = 3
 
 class PostAdmin(admin.ModelAdmin):
-	prepopulated_fields = {"slug": ("title",)}
+  class Meta:
+    model = Post
+
+  fieldsets = [
+    ('Post Content', {'fields': ['title', 'text']}),
+    ('Post Info', {'fields':['pub_date', 'slug', 'tags']}),
+  ]
+  # inlines = [TagInline]
+  filter_horizontal = ['tags']
+  list_display = ('title', 'pub_date')
+  prepopulated_fields = {"slug": ("title",)}
 
 # Register your models here.
+admin.site.register(Tag, TagAdmin)
 admin.site.register(Post, PostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -4,12 +4,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	# get the blog posts that are published
-	posts = Post.objects.all
-	# now return the rendered template
-	return render(request, 'blog/post_list.html', {'object_list': posts})
+  # get the blog posts that are published
+  posts = Post.objects.all
+  # now return the rendered template
+  return render(request, 'blog/post_list.html', {'object_list': posts})
 
 def post_detail(request, year, month, slug):
-	unslug = slug.replace('-', ' ')
-	post = Post.objects.get(slug=str(slug))
-	return render(request, 'blog/post_detail.html', {'post': post})
+  unslug = slug.replace('-', ' ')
+  post = Post.objects.get(slug=str(slug))
+  return render(request, 'blog/post_detail.html', {'post': post})
+
+def tagged(request, tag):
+  posts = Post.objects.filter(tags__slug=tag)
+  return render(request, 'blog/post_list.html', {'object_list': posts})
