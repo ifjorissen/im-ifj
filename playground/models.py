e5484e15efb17193c6afce129447a756d0963dd4
Isabella Jorissen <ifjorissen@gmail.com>
2015-07-16 15:07:07 -0700

    added a gallery option for the playground

--- playground/models.py
+++ playground/models.py
@@ -24,21 +24,15 @@ class Project(models.Model):
   #to do: multiple image uploads
   title = models.CharField(max_length = 200)
   pub_date = models.DateTimeField('date added to site')
-  projurl = models.URLField()
+  projurl = models.URLField(null=True, blank=True)
   completion_date = models.DateTimeField('date completed')
-  # thumbnail = models.ForeignKey(ProjectImage, null=True)
   thumbnail = models.ImageField(upload_to=thumbnail_image_path, null=True)
-  # tech = models.ManyToManyField(ProjectTech)
-  # color = models.ManyToManyField(ProjectColor)
-  # projimg = models.ImageField(upload_to='playground/%Y/%m/%d', null=True)
-  objective = models.TextField()
+  objective = models.TextField(null=True, blank=True)
   description = models.TextField()
-  # motivation = models.TextField()
   solution = models.TextField(null = True, blank=True)
-  # tech = models.TextField(null=True)
-  #add colors, fonts? class/company? known bugs?
   slug = models.SlugField(max_length = 40, unique = True)
-  
+  gallery = models.BooleanField(default=False)
+
   def get_absolute_url(self):
     return "/playground/%s/" %(self.slug)
 
@@ -52,14 +46,12 @@ class Project(models.Model):
 #these could be many to many relationships instead to speed up the data
 class ProjectTech(models.Model):
   project = models.ForeignKey(Project, null = True)
-  # projects = models.ManyToManyField(Project, null=True)
   name = models.CharField(max_length = 200)
   # def __unicode__(self):
   #   return self.name
 
 class ProjectColor(models.Model):
   project = models.ForeignKey(Project, null = True)
-  # proj = models.ManyToManyField(Project)
   hexa = models.TextField(max_length=10)
     # def __unicode__(self):
   #   return self.color
